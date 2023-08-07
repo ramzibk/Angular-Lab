8fefb8d0c6ef0356f0cca6ee5f79651b9db8a45d
ramzibk <bk.ramzi@gmail.com>
2023-08-07 17:08:36 +0200

    generate counter component

--- state-mgnt/src/app/app.module.ts
+++ state-mgnt/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
