a361be6001fce8f144770e54cd62962f0e4299d9
ramzibk <bk.ramzi@gmail.com>
2023-08-10 00:19:54 +0200

    implement book list page

--- bookStore/src/app/book-list/book-list.component.ts
+++ bookStore/src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './books.model';
 
 @Component({
   selector: 'app-book-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent {
+
+  books : Array<Book> = []
+
+  constructor() {
+    let book1 : Book = {id: "1001", volumeInfo: {title: "Book 1", authors : ["John Doe"]}}
+    let book2 : Book = {id: "1002", volumeInfo: {title: "Book 2", authors : ["John Dark"]}}
+    this.books.push(book1)
+    this.books.push(book2)
+  }
 
 }
