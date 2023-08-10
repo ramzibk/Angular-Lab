751e83b25ebb9c8b3f14512a05727284f95468af
ramzibk <bk.ramzi@gmail.com>
2023-08-11 01:00:30 +0200

    implement book-collection content

--- bookStore/src/app/book-collection/book-collection.component.ts
+++ bookStore/src/app/book-collection/book-collection.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Book } from '../book-list/books.model';
 
 @Component({
   selector: 'app-book-collection',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class BookCollectionComponent {
 
+  @Input()
+  books: Array<Book> = [];
+
+  ngOnInit() {
+    let book1 : Book = {id: "001", volumeInfo: {title: "book1", authors: ["Ryan James"]}};
+    this.books.push(book1);
+  }
 }
